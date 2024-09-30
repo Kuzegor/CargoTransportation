d3d45d906fce2a4c2e09498bfe3de130797e42eb
Kuzegor <kuznetcov03@inbox.ru>
2024-09-30 12:57:29 +0300

    front

--- CargoTransportation/wwwroot/js/site.js
+++ CargoTransportation/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//function showMenuPopup() {
+//    let menuPopup = document.getElementById("menu-popup");
+//    let blankCanvas = document.getElementById("blank-canvas");
+//    menuPopup.style.visibility = "visible";
+//    blankCanvas.style.visibility = "visible";
+//}
+
+//function hideMenuPopup() {
+//    let userPopup = document.getElementById("menu-popup");
+//    let blankCanvas = document.getElementById("blank-canvas");
+//    userPopup.style.visibility = "hidden";
+//    blankCanvas.style.visibility = "hidden";
+//}
